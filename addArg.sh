042b39150ab8d2a298531b7f25dcfa9da7a9db8d
rx5corp <80647392+rx5corp@users.noreply.github.com>
2021-03-14 20:20:37 +0000

    Create addArg.sh

---
+++ addArg.sh
@@ -0,0 +1,23 @@
+
+client=$1
+cd /etc/openvpn/server/easy-rsa/
+EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full "$client" nopass
+new_client () {
+	# Generates the custom client.ovpn
+	{
+	cat /etc/openvpn/server/client-common.txt
+	echo "<ca>"
+	cat /etc/openvpn/server/easy-rsa/pki/ca.crt
+	echo "</ca>"
+	echo "<cert>"
+	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt
+	echo "</cert>"
+	echo "<key>"
+	cat /etc/openvpn/server/easy-rsa/pki/private/"$client".key
+	echo "</key>"
+	echo "<tls-crypt>"
+	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
+	echo "</tls-crypt>"
+	} > /var/www/html/"$client".ovpn
+}
+new_client
