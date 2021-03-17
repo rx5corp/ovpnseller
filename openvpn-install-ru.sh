bba3133661019c0d3bfb9462cddff31f96c3e76f
rx5corp <80647392+rx5corp@users.noreply.github.com>
2021-03-18 02:21:17 +0500

    Update openvpn-install-ru.sh

--- openvpn-install-ru.sh
+++ openvpn-install-ru.sh
@@ -314,7 +314,7 @@ server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
 		;;
 	esac
 	echo "keepalive 10 120
-cipher AES-256-CBC
+cipher ChaCha20-Poly1305
 user nobody
 group $group_name
 persist-key
@@ -415,7 +415,7 @@ persist-key
 persist-tun
 remote-cert-tls server
 auth SHA512
-cipher AES-256-CBC
+cipher ChaCha20-Poly1305
 ignore-unknown-option block-outside-dns
 block-outside-dns
 verb 3" > /etc/openvpn/server/client-common.txt
